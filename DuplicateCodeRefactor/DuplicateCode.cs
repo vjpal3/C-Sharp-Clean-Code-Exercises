063c32e49623c7be3de81d9ac72f95deeb1b4af5
vjpal3 <vrishalipal@yahoo.com>
2019-09-02 17:56:59 -0400

    Initial Commit

---
+++ DuplicateCodeRefactor/DuplicateCode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeExercises.DuplicateCodeRefactor
+{
+    class DuplicateCode
+    {
+        public void AdmitGuest(string name, string admissionDateTime)
+        {
+            // .....
+            // Business Logic 
+
+            int time;
+            int hours = 0, minutes = 0;
+            if (!string.IsNullOrEmpty(admissionDateTime)) {
+                if(int.TryParse(admissionDateTime.Replace(":", ""), out time))
+                {
+                    hours = time / 100;
+                    minutes = time % 100;
+
+                    Console.WriteLine("Parsed Time: hours: {0}, Minutes: {1}", hours, minutes);
+                }
+                else
+                {
+                    throw new ArgumentNullException("AdmissionDateTime");
+                }
+
+            }
+            else
+                throw new ArgumentNullException("AdmissionDateTime");
+
+            // .....
+            // More Business Logic 
+        }
+
+        public void UpdateAdmission(int admissionId, string name, string admissionDateTime)
+        {
+            // .....
+            // Business Logic 
+
+            int time;
+            int hours = 0, minutes = 0;
+            if (!string.IsNullOrEmpty(admissionDateTime))
+            {
+                if (int.TryParse(admissionDateTime.Replace(":", ""), out time))
+                {
+                    hours = time / 100;
+                    minutes = time % 100;
+                    Console.WriteLine("Parsed Time: hours: {0}, Minutes: {1}", hours, minutes);
+                }
+                else
+                {
+                    throw new ArgumentNullException("AdmissionDateTime");
+                }
+
+            }
+            else
+                throw new ArgumentNullException("AdmissionDateTime");
+
+            // .....
+            // More Business Logic 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanCodeExercises.DuplicateCodeRefactor;
 using CleanCodeExercises.SwitchStatementRefacor;
+
 
 namespace CleanCodeExercises
 {
@@ -11,15 +13,20 @@ namespace CleanCodeExercises
     {
         static void Main(string[] args)
         {
-            var customer1 = new PayAsYouGoCustomer();
-            var usage1 = new MonthlyUsage { CallMinutes = 130, SmsCount = 213, Customer = customer1 };           
-            var statement1 = usage1.Customer.GenerateStatement(usage1);
-            statement1.PrintStatement();
+            //Run DuplicateCodeRefactor Code
+            new DuplicateCode().AdmitGuest("John Smith", "17:10");
+            new DuplicateCode().UpdateAdmission(10, "Jane Parera", "12:40");
 
-            var customer2 = new UnlimitedCustomer();
-            var usage2 = new MonthlyUsage { CallMinutes = 100, SmsCount = 100, Customer = customer2 };
-            var statement2 = usage2.Customer.GenerateStatement(usage2);
-            statement2.PrintStatement();
+            //Run SwitchStatementRefactor Code
+            //var customer1 = new PayAsYouGoCustomer();
+            //var usage1 = new MonthlyUsage { CallMinutes = 130, SmsCount = 213, Customer = customer1 };           
+            //var statement1 = usage1.Customer.GenerateStatement(usage1);
+            //statement1.PrintStatement();
+
+            //var customer2 = new UnlimitedCustomer();
+            //var usage2 = new MonthlyUsage { CallMinutes = 100, SmsCount = 100, Customer = customer2 };
+            //var statement2 = usage2.Customer.GenerateStatement(usage2);
+            //statement2.PrintStatement();
         }
     }
 }
