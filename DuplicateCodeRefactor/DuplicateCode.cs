2836ddcd69428a40a157ccba65faae5ce7f2ed53
vjpal3 <vrishalipal@yahoo.com>
2019-09-02 18:55:16 -0400

    Replace duplicate code with a method that returns an object

--- DuplicateCodeRefactor/DuplicateCode.cs
+++ DuplicateCodeRefactor/DuplicateCode.cs
@@ -6,41 +6,32 @@ using System.Threading.Tasks;
 
 namespace CleanCodeExercises.DuplicateCodeRefactor
 {
-    class DuplicateCode
+    public class Time
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public Time(int hours, int minutes)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+        }
+    }
+    public class DuplicateCode
     {
         public void AdmitGuest(string name, string admissionDateTime)
         {
             // .....
-            // Business Logic 
-
-            int time;
-            int hours = 0, minutes = 0;
-            if (!string.IsNullOrEmpty(admissionDateTime)) {
-                if(int.TryParse(admissionDateTime.Replace(":", ""), out time))
-                {
-                    hours = time / 100;
-                    minutes = time % 100;
-
-                    Console.WriteLine("Parsed Time: hours: {0}, Minutes: {1}", hours, minutes);
-                }
-                else
-                {
-                    throw new ArgumentNullException("AdmissionDateTime");
-                }
-
-            }
-            else
-                throw new ArgumentNullException("AdmissionDateTime");
+            // Business Logic             
+            var time = GetTime(admissionDateTime);
+            var hours = time.Hours;
+            var minutes = time.Minutes;
 
             // .....
             // More Business Logic 
         }
 
-        public void UpdateAdmission(int admissionId, string name, string admissionDateTime)
+        private static Time GetTime(string admissionDateTime)
         {
-            // .....
-            // Business Logic 
-
             int time;
             int hours = 0, minutes = 0;
             if (!string.IsNullOrEmpty(admissionDateTime))
@@ -49,6 +40,7 @@ namespace CleanCodeExercises.DuplicateCodeRefactor
                 {
                     hours = time / 100;
                     minutes = time % 100;
+
                     Console.WriteLine("Parsed Time: hours: {0}, Minutes: {1}", hours, minutes);
                 }
                 else
@@ -57,8 +49,20 @@ namespace CleanCodeExercises.DuplicateCodeRefactor
                 }
 
             }
-            else
+            else 
                 throw new ArgumentNullException("AdmissionDateTime");
+
+            return new Time(hours, minutes);
+        }
+
+        public void UpdateAdmission(int admissionId, string name, string admissionDateTime)
+        {
+            // .....
+            // Business Logic 
+          
+            var time = GetTime(admissionDateTime);
+            var hours = time.Hours;
+            var minutes = time.Minutes;
 
             // .....
             // More Business Logic 
