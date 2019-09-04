4c6ef3a515bc8a80f2ee5d532454ac2b0a7b1508
vjpal3 <vrishalipal@yahoo.com>
2019-09-04 18:20:08 -0400

    Initial Commit

---
+++ MagicNumbersRefactor/MagicNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeExercises.MagicNumbersRefactor
+{
+    class MagicNumbers
+    {
+        public void ApproveDocument(int status)
+        {
+            if(status == 1)
+            {
+                // ....
+            }
+            else if (status == 2)
+            {
+                // ....
+            }
+        }
+
+        public void RejectDocument(string status)
+        {
+            switch(status)
+            {
+                case "1":
+                    // ...
+                    break;
+                case "2":
+                    // ...
+                    break;
+            }
+        }
+    }
+}
