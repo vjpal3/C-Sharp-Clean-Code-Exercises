c9f9b65aa6db7afebd663ccc5d4a1b46a4e6b1b9
vjpal3 <vrishalipal@yahoo.com>
2019-09-04 22:37:06 -0400

    Initial Commit

---
+++ NestedConditionalsRefactor/Reservasion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeExercises.NestedConditionalsRefactor
+{
+    class Customer
+    {
+        public int LoyaltyPoints { get; set; }
+    }
+
+    class Reservasion
+    {
+        public DateTime From { get; set; }
+        public Customer Customer { get; set; }
+        public bool IsCancelled { get; set; }
+
+        public Reservasion(Customer customer, DateTime datetime)
+        {
+            From = datetime;
+            Customer = customer;
+        }
+
+        public void Cancel()
+        {
+            // Gold Customer can cancel upto 24 hrs before
+            if(Customer.LoyaltyPoints > 100)
+            {
+                try
+                {
+                    if (DateTime.Now > From)
+                        throw new InvalidOperationException("It's too late to cancel. ");
+
+                    if ((From - DateTime.Now).TotalHours < 24)
+                        throw new InvalidOperationException("It's too late to cancel. ");
+                    
+                }
+                catch(InvalidOperationException e)
+                {
+                    Console.Write(e.Message);
+                    IsCancelled = false;
+                    return;
+                }
+                IsCancelled = true;
+
+            }
+            else
+            {
+                // Regular Customer can cancel upto 48 hrs before
+                try
+                {
+                    if (DateTime.Now > From)
+                        throw new InvalidOperationException("It's too late to cancel.");
+
+                    if ((From - DateTime.Now).TotalHours < 48)
+                        throw new InvalidOperationException("It's too late to cancel.");
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.Write(e.Message);
+                    IsCancelled = false;
+                    return;
+                }
+
+                IsCancelled = true;
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CleanCodeExercises.DuplicateCodeRefactor;
 using CleanCodeExercises.SwitchStatementRefacor;
+using CleanCodeExercises.NestedConditionalsRefactor;
 
 
 namespace CleanCodeExercises
@@ -13,9 +14,42 @@ namespace CleanCodeExercises
     {
         static void Main(string[] args)
         {
+            //Run NestedConditionalRefactor Code
+            var goldCustomer1 = new CleanCodeExercises.NestedConditionalsRefactor.Customer() { LoyaltyPoints = 120 };
+            var reservation1 = new Reservasion(goldCustomer1, DateTime.Now.AddHours(25));
+            reservation1.Cancel();
+            Console.WriteLine("Gold Customer, 25 Hrs before - Cancellation status: " + reservation1.IsCancelled);
+            Console.WriteLine();
+          
+            reservation1 = new Reservasion(goldCustomer1, DateTime.Now.AddHours(23));
+            reservation1.Cancel();
+            Console.WriteLine("Gold Customer, less than 24 Hrs - Cancellation status: " + reservation1.IsCancelled);
+            Console.WriteLine();
+
+            reservation1 = new Reservasion(goldCustomer1, DateTime.Now.AddDays(-1));
+            reservation1.Cancel();
+            Console.WriteLine("Gold Customer, cancelling after start date - Cancellation status: " + reservation1.IsCancelled);
+            Console.WriteLine();
+
+            var regularCustomer1 = new CleanCodeExercises.NestedConditionalsRefactor.Customer() { LoyaltyPoints = 90 };
+            var reservation2 = new Reservasion(regularCustomer1, DateTime.Now.AddHours(49));
+            reservation2.Cancel();
+            Console.WriteLine("Regular Customer, 49 Hrs before - Cancellation status: " + reservation2.IsCancelled);
+            Console.WriteLine();
+
+            reservation2 = new Reservasion(regularCustomer1, DateTime.Now.AddHours(47));
+            reservation2.Cancel();
+            Console.WriteLine("Regular Customer, less than 48 Hrs - Cancellation status: " + reservation2.IsCancelled);
+            Console.WriteLine();
+
+            reservation2 = new Reservasion(regularCustomer1, DateTime.Now.AddDays(-1));
+            reservation2.Cancel();
+            Console.WriteLine("Regular Customer, cancelling after start date - Cancellation status: " + reservation2.IsCancelled);
+
+
             //Run DuplicateCodeRefactor Code
-            new DuplicateCode().AdmitGuest("John Smith", "17:10");
-            new DuplicateCode().UpdateAdmission(10, "Jane Parera", "12:27");
+            //new DuplicateCode().AdmitGuest("John Smith", "17:10");
+            //new DuplicateCode().UpdateAdmission(10, "Jane Parera", "12:27");
 
             //Run SwitchStatementRefactor Code
             //var customer1 = new PayAsYouGoCustomer();
